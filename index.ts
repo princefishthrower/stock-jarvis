64a7508b557cfee212c9f39f70d70a0ca1e5326e
Chris Frewin <frewin.christopher@gmail.com>
2020-03-31 20:25:58 +0200

    wow, i must be brain dead

--- index.ts
+++ index.ts
@@ -10,14 +10,16 @@ const app = new App();
 if (env.NODE_ENV === "DEVELOP") {
     app.runAudioUpdate();
 } else {
+    // At most once per hour (0 in minute component) OR
     // “At every nth minute past every hour from 9 through 18 on every day-of-week from Monday through Friday.”
-    const audioUpdateMinuteInterval = settings.audioUpdate.minuteInterval >= 60 ? "0" : "/" + settings.audioUpdate.minuteInterval.toString();
+    const audioUpdateMinuteInterval = settings.audioUpdate.minuteInterval >= 60 ? "0" : "*/" + settings.audioUpdate.minuteInterval.toString();
     cron.schedule(audioUpdateMinuteInterval + " 9-18 * * 1-5", async () => {
         app.runAudioUpdate();
     });
 
+    // At most once per hour (0 in minute component) OR
     // “At every nth minute past every hour from 9 through 18 on every day-of-week from Monday through Friday.”
-    const notificationUpdateMinuteInterval = settings.notificationUpdate.minuteInterval >= 60 ? "0" : "/" + settings.notificationUpdate.minuteInterval.toString();
+    const notificationUpdateMinuteInterval = settings.notificationUpdate.minuteInterval >= 60 ? "0" : "*/" + settings.notificationUpdate.minuteInterval.toString();
     cron.schedule(notificationUpdateMinuteInterval + " 9-18 * * 1-5", async () => {
         app.runNotificationUpdate();
     });
